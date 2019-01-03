3783baea2ae4124ad8a9a164e5654751f9d5a6a4
annabytnar <akbytnar@gmail.com>
2019-01-03 14:52:07 +0100

    scroll in eventListener

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
+// Initial state
+var scrollPos = 0;
 const contentBox = document.getElementById("contentBox");
 const contentBoxMargin = window.getComputedStyle(contentBox).marginTop.substring(0, window.getComputedStyle(contentBox).marginTop.length - 2);
 const contentBoxPosition = contentBox.offsetTop - contentBoxMargin;
 
-var scrollDown=false;
-function scrollToContent (){
-    //setTimeout(function () {
-        if(window.scrollY < contentBoxPosition && scrollDown){
-            console.log('pos');
-            window.scrollBy(
-                {top: contentBoxPosition}
-            );
-    }
+var scrollDown = false;
 
-   // }, 50);
+function isScrollDown() {
+    if ((document.body.getBoundingClientRect()).top > scrollPos) {
+        scrollDown = false;
+    }
+    else {
+        scrollDown = true;
+    }
 }
 
-// Initial state
-var scrollPos = 0;
-// adding scroll event
-window.addEventListener('scroll', function(){
-    // detects new state and compares it with the new one
-    if ((document.body.getBoundingClientRect()).top > scrollPos){
-        scrollDown=false;
-    }
-    else
-    {
-        scrollDown=true;
-    }
-    // saves the new position for iteration.
+window.addEventListener('scroll', function () {
+    isScrollDown();
     scrollPos = (document.body.getBoundingClientRect()).top;
+    if (window.scrollY < contentBoxPosition && scrollDown) {
+     contentBox.scrollIntoView();
+    }
 });
-
-
-
-// function scrollToContent (){
-//     if (!action){
-//         action = true;
-//         console.log('action ashjdbdsjd');
-//         setTimeout(function () {
-//             if(window.scrollY <= contentBoxPosition){
-//                 window.scrollTo(
-//                     {top: contentBoxPosition,
-//                         behavior: 'smooth'}
-//                 )
-//             }
-//         }, 5);
-//     }
-//
-// }
 
